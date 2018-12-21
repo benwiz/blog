a49fe57b1c559ab3a699e8f9fee9a4bb69b8a2ca
Ben Wiz <bwisialowski@gmail.com>
2018-12-21 08:20:24 -0800

    untest

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# NOTE: Currently I'm not using any plugins unsupported by github, so I can just push normal code straight to master.
+
 sudo rm -r _site/
 git clone -b gh-pages `git config remote.origin.url` _site
 bundle exec jekyll build
